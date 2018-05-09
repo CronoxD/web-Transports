ab9254636f59d15b43588cae21a91d0c2a93fb97
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2018-05-08 22:03:14 -0500

    The responsive menu and the button menu were added

--- js/master.js
+++ js/master.js
@@ -3,11 +3,21 @@ $(document).ready(function(){
   const buscador = $('.buscador');
   const iconSearch = $('.icon-search');
   const iconCancel = $('.icon-cancel');
-  //Oculta el buscador al cargar la página
-  const toggleSearch = (ms) => buscador.fadeToggle(ms);
-  toggleSearch(0);
-  //Si se hace un clic en el boton search aparece el buscador
-  iconSearch.click(toggleSearch.bind(null,500));
-  iconCancel.click(toggleSearch.bind(null,500));
+  const iconMenu = $('.icon-menu');
+  const menu = $('.menu');
+  //Crear media query
+  const screenMin900 = window.matchMedia("(min-width: 900px)");
+  //Oculta el buscador al cargar la página si el ancho es mayor a 900px
+  if(screenMin900.matches) {
+    const toggleSearch = (ms) => buscador.fadeToggle(ms);
+    toggleSearch(0);
+    //Si se hace un clic en el boton search aparece el buscador
+    iconSearch.click(toggleSearch.bind(null,500));
+    iconCancel.click(toggleSearch.bind(null,500));
+  } else { //Si la pantalla es menor a 900, se activa el boton del menu
+    iconMenu.click(function(){
+      menu.slideToggle('slow');
+    });
+  }
 
 });
